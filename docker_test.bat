5b86940602d45a1e318b938b3b3fbb047ecaf4bb
Albert Ang <73264977+albert-a0@users.noreply.github.com>
2025-12-29 18:24:59 -0500

    thisis commit

--- docker_test.bat
+++ docker_test.bat
@@ -1,5 +1,12 @@
+REM This is a test Docker batch file
+
+REM  just to test if the command will output correctly
 echo hello world
 
+REM Make sure the device has Docker installed and running
+docker --version
+
+REM List all Docker containers
 docker ps --all
 
 pause
